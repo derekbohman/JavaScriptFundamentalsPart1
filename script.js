d28acb23077930455a48dcac27bf032da4d136bb
Derek <derekbohman@gmail.com>
2022-11-11 17:10:52 -0800

    Learning.

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 let js = "amazing";
 if (js === "amazing") alert("JavaScript is FUN!");
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
